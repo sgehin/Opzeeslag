a936e06d6fcc6203a32287a70e2f8381b4016d88
sgehin <sfa.gehin@gmail.com>
2017-11-10 21:59:09 +0100

    ship added with borrowed sunk function

--- index.php
+++ index.php
@@ -12,23 +12,43 @@ and open the template in the editor.
             td {
                 background-color: lightskyblue;
                 width: 50px;
-                height: 50px;   
+                height: 50px;
+                
             }
             </style>
+            
         <script>
-                function fire(q){
-                 alert('q');
-                 document.getElementById("myPos"+q).style.backgroundColor = "red";
+            var ship = ["22", "23", "24"];
+            function fire(q){
+                if(q==ship[0] || q ==ship[1] || q ==ship[2] ){
+                    document.getElementById("myPos"+q).style.backgroundColor = 'red';                    
+                    if(checkIfShipSunk(ship)){
+                        alert("Ship has sunk");
+                    }
+                }else{
+                    document.getElementById("myPos"+q).style.backgroundColor = 'blue';                    
+                   
                 }
+            }
+            function checkIfShipSunk(shipvar){
+               for(xvar = 0; xvar < shipvar.length; xvar++ ){
+                   var dekleur = document.getElementById('myPos'+shipvar[xvar]).style.backgroundColor;
+                   alert('myPos'+shipvar[xvar]);
+                    if(dekleur != 'red'){
+                       return false;
+                   }
+                } 
+                return true;
+            }
         </script>    
         
     </head>
     <body>
         <table>
                 <?php
-                for($x=1;$x<3;$x++){
+                for($x=1;$x<10;$x++){
                     echo "<tr>";
-                    for($y=1;$y<3;$y++){
+                    for($y=1;$y<10;$y++){
                         echo "<td  id=myPos$x$y onclick=fire($x$y)>x</td>";
                     }
                     "</tr>";
@@ -37,3 +57,4 @@ and open the template in the editor.
         </table>    
     </body>
 </html>
+
